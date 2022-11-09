85afd161e6447a7228208f798fde7a37b49aedee
CaioHenriqueSousa <106630394+CaioHenriqueSousa@users.noreply.github.com>
2022-11-09 20:06:26 -0300

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -199,7 +199,7 @@ class Game {
       collected.remove()
    })
    if(player.fuel > 0 && !this.playerMoving) {
-    player.fuel -=0.4
+    player.fuel -=0.2
    }
   if(player.fuel <= 0){
     gameState = 2
